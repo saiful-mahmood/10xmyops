0b14ccb0a4e4f0f7860aa15fc74570ce8d80d2f3
saiful-mahmood <76782673+saiful-mahmood@users.noreply.github.com>
2026-04-07 20:47:02 -0400

    Update configurations to use custom domain 10xmyops.com

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/10xmyops/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
